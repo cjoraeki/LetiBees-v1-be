62e3ff0ae0c1a9ac156d50cf3cbc4af5f0c154ce
cjoraeki <cjoraeki3@gmail.com>
2023-02-15 11:01:36 +0100

    first commit

---
+++ src/main/java/com/example/LetiBeesv1be/LetiBeesV1BeApplication.java
@@ -0,0 +1,13 @@
+package com.example.LetiBeesv1be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetiBeesV1BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetiBeesV1BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LetiBeesv1be/LetiBeesV1BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LetiBeesv1be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetiBeesV1BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
